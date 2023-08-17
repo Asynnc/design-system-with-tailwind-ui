9c05b99b046b32493c42d97f46c2f997b334cb4b
Antonio Silva <antoniobsilvaprof@hotmail.com>
2023-08-17 15:35:02 -0300

    chore: add photo section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Mail } from 'lucide-react'
+import { Mail, UploadCloud, User } from 'lucide-react'
 import { Input } from './components/Input'
 import { SettingsTabs } from './components/Tabs/SettingsTabs'
 import { Divider } from './components/Sidebar/Divider'
@@ -92,7 +92,30 @@ export default function Home() {
                 This will be displayed on your profile.{' '}
               </span>
             </label>
-            <div className="grid grid-cols-2 gap-6"></div>
+            <div className="flex items-start gap-5">
+              <div className="flex h-16 w-16 items-center justify-center rounded-full bg-green-50">
+                <User className="h-8 w-8 text-green-500" />
+              </div>
+              <label
+                htmlFor="photo"
+                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-md border border-dashed border-zinc-200 px-6 py-4 text-center text-zinc-500 shadow-sm hover:bg-green-100"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-green-50 group-hover:bg-white">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-green-700" />
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    {' '}
+                    <span className="font-semibold text-green-700">
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className="text-sm">PNG or JPG (max. 800x400px)</span>
+                </div>
+              </label>
+              <input type="file" className="sr-only" id="photo" />
+            </div>
           </div>
           <Divider />
           <div className="grid grid-cols-form gap-3">
@@ -161,7 +184,6 @@ export default function Home() {
             </label>
             <div className="grid grid-cols-2 gap-6"></div>
           </div>
-
           <div className="flex items-center justify-end gap-2 pt-5">
             <button
               type="button"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      borderWidth: {
+        6: '6px',
+      },
     },
   },
   plugins: [],
